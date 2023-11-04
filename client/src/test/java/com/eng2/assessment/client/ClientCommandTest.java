1a3029973b76e7b3837eb0c0428b390145cf2465
dont-text-me <ivanbar3330@gmail.com>
2023-11-04 02:16:16 +0000

    setup subproject

---
+++ client/src/test/java/com/eng2/assessment/client/ClientCommandTest.java
@@ -0,0 +1,27 @@
+package com.eng2.assessment.client;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+public class ClientCommandTest {
+
+  @Test
+  public void testWithCommandLineOption() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(baos));
+
+    try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+      String[] args = new String[] {"-v"};
+      PicocliRunner.run(ClientCommand.class, ctx, args);
+
+      // client
+      assertTrue(baos.toString().contains("Hi!"));
+    }
+  }
+}
