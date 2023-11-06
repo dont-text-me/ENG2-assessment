3c4a862f98080c21445235eece0e2c5e8ebd56fb
dont-text-me <ivanbar3330@gmail.com>
2023-11-06 20:07:16 +0000

    set java version in subprojects

--- client/src/main/java/com/eng2/assessment/client/ClientCommand.java
+++ client/src/main/java/com/eng2/assessment/client/ClientCommand.java
@@ -6,11 +6,11 @@ import com.eng2.assessment.client.commands.vm.users.RegisterUserCommand;
 import com.eng2.assessment.client.commands.vm.videos.*;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 
 @Command(
     name = "client",
-    description = "...",
+    description =
+        "The root command for the video platform. Please use the subcommands to interact with videos.",
     mixinStandardHelpOptions = true,
     subcommands = {
       ListUsersCommand.class,
@@ -23,20 +23,9 @@ import picocli.CommandLine.Option;
       ListTrendingHashtagsCommand.class
     })
 public class ClientCommand implements Runnable {
-
-  @Option(
-      names = {"-v", "--verbose"},
-      description = "...")
-  boolean verbose;
-
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     PicocliRunner.run(ClientCommand.class, args);
   }
 
-  public void run() {
-    // business logic here
-    if (verbose) {
-      System.out.println("Hi!");
-    }
-  }
+  public void run() {}
 }
