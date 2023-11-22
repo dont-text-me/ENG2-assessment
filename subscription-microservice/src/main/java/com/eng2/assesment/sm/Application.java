aa513ce1958d5cae2bd4ce837e2d5ea8fba664b2
dont-text-me <ivanbar3330@gmail.com>
2023-11-22 14:20:36 +0000

    setup sm project

---
+++ subscription-microservice/src/main/java/com/eng2/assesment/sm/Application.java
@@ -0,0 +1,10 @@
+package com.eng2.assesment.sm;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
