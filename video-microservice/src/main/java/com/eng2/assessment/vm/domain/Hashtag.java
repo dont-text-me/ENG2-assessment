d5114c20533af3ebf6b728a35e3b4fa3530fc389
dont-text-me <ivanbar3330@gmail.com>
2023-11-23 18:29:46 +0000

    format

--- video-microservice/src/main/java/com/eng2/assessment/vm/domain/Hashtag.java
+++ video-microservice/src/main/java/com/eng2/assessment/vm/domain/Hashtag.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micronaut.serde.annotation.Serdeable;
 import jakarta.persistence.*;
 import java.util.Set;
-
-@Entity
-@Serdeable
 /**
  * Hashtag entity. Hashtag names have to be unique to be able to correctly identify videos,
  * therefore its name is its identifier in the database.
  */
+@Entity
+@Serdeable
 public class Hashtag {
   @Id private String id;
 
