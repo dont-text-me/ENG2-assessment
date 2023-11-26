d04235592f7b6c19941722dbd1c23d7e532b87f1
dont-text-me <ivanbar3330@gmail.com>
2023-11-26 23:15:15 +0000

    send message when user is registered

---
+++ subscription-microservice/src/main/java/com/eng2/assessment/sm/events/UserCreationConsumer.java
@@ -0,0 +1,32 @@
+package com.eng2.assessment.sm.events;
+
+import static com.eng2.assessment.vm.events.Topics.TOPIC_USER_REGISTERED;
+
+import com.eng2.assessment.sm.domain.User;
+import com.eng2.assessment.sm.repositories.UserRepository;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.KafkaListener;
+import io.micronaut.configuration.kafka.annotation.Topic;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import java.util.HashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@KafkaListener(groupId = "subscription-microservice")
+public class UserCreationConsumer {
+  private static final Logger logger = LoggerFactory.getLogger(UserCreationConsumer.class);
+  @Inject private UserRepository userRepository;
+
+  @Topic(TOPIC_USER_REGISTERED)
+  @Transactional
+  void processUserRegistered(@KafkaKey String userName) {
+    if (!userRepository.existsByUserNameEqual(userName)) {
+      logger.info("Creating new user with username " + userName);
+      User newUser = new User();
+      newUser.setUserName(userName);
+      newUser.setViewedVideos(new HashSet<>());
+      userRepository.save(newUser);
+    }
+  }
+}
---
+++ video-microservice/src/main/java/com/eng2/assessment/vm/events/UserCreationProducer.java
@@ -0,0 +1,13 @@
+package com.eng2.assessment.vm.events;
+
+import static com.eng2.assessment.vm.events.Topics.TOPIC_USER_REGISTERED;
+
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import io.micronaut.configuration.kafka.annotation.KafkaKey;
+import io.micronaut.configuration.kafka.annotation.Topic;
+
+@KafkaClient
+public interface UserCreationProducer {
+  @Topic(TOPIC_USER_REGISTERED)
+  void userRegistered(@KafkaKey String userName);
+}
