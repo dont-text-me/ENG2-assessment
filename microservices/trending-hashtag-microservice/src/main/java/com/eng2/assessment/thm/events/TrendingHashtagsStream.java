100d894e48e1dbd21ec3ca8f3f5a418e32d1504f
dont-text-me <ivanbar3330@gmail.com>
2023-12-23 21:36:21 +0300

    update doc link

--- microservices/trending-hashtag-microservice/src/main/java/com/eng2/assessment/thm/events/TrendingHashtagsStream.java
+++ microservices/trending-hashtag-microservice/src/main/java/com/eng2/assessment/thm/events/TrendingHashtagsStream.java
@@ -35,7 +35,7 @@ public class TrendingHashtagsStream implements ITrendingHashtagsStream {
    * one record, i.e. if a video with 5 hashtags is liked, 5 internal messages are produced for each
    * of the hashtags. Then, the likes for each hashtag are summed over a time window and passed
    * forward once the window closes. Further processing is done in {@link
-   * TrendingHashtagSummaryConsumer}, which stores a {@link thm.domain.TrendingHashtag} for each
+   * TrendingHashtagSummaryConsumer}, which stores a {@link com.eng2.assessment.generated.thm.domain.TrendingHashtag} for each
    * summarized message i.e. for each hashtag's like count over the time window.
    */
   @Singleton
